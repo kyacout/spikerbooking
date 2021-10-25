a8a46266d1ff2be4e4a2a232699ea35d7a56c9a3
Khaled Yacout <khaled.yacout@gmail.com>
2021-10-25 15:36:30 +0200

    add base layout jsx file

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,36 +1,18 @@
 import React from 'react'
 import { createContext } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import CssBaseline from '@mui/material/CssBaseline'
-import { createTheme } from '@mui/material/styles'
-import { ThemeProvider } from '@mui/material/styles'
 
 import Routes from './Routes'
-
-const theme = createTheme({
-  palette: {
-    type: 'light',
-    primary: {
-      main: '#d32531',
-    },
-    secondary: {
-      main: '#000000',
-    },
-  },
-})
 
 export const Context = createContext({ currentUser: { id: null, email: null, current_type: null }, token: null })
 
 const App = ({ currentUser, token }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router>
-        <Context.Provider value={{ currentUser, token }}>
-          <Routes />
-        </Context.Provider>
-      </Router>
-    </ThemeProvider>
+    <Router>
+      <Context.Provider value={{ currentUser, token }}>
+        <Routes />
+      </Context.Provider>
+    </Router>
   )
 }
 
--- app/javascript/layouts/FixedBackground/index.jsx
+++ app/javascript/layouts/FixedBackground/index.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 import { imageURL } from '../../helpers/cloudinary'
 import styles from './styles.module.scss'
+import { Base } from '../Base'
 
 export const FixedBackground = ({ bgImg, children }) => {
   return (
-    <>
+    <Base>
       <img src={bgImg} className={styles.bg} alt="" />
       <div className="bgLogoContainer">
         <img src={imageURL('v1634557521/white_logo.png')} className={styles.bgLogoContainer} alt="" />
       </div>
       {children}
-    </>
+    </Base>
   )
 }
