4eef0948c394fc825c24e0d74b2818fa697436d2
Khaled Yacout <khaled.yacout@gmail.com>
2021-11-03 18:16:51 +0200

    Artist profile page 1

--- app/javascript/pages/CreateArtistProfile/index.jsx
+++ app/javascript/pages/CreateArtistProfile/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { useFormik } from 'formik'
 import * as yup from 'yup'
 import TextField from '@mui/material/TextField'
@@ -12,14 +12,18 @@ import InputLabel from '@mui/material/InputLabel'
 import Select from '@mui/material/Select'
 import Box from '@mui/material/Box'
 import Chip from '@mui/material/Chip'
-import InputAdornment from '@mui/material/InputAdornment'
 import OutlinedInput from '@mui/material/OutlinedInput'
-import PhotoCamera from '@mui/icons-material/PhotoCamera'
 
 import styles from './styles.module.scss'
+import { Context } from '../../components/App'
 import { FixedBackgroundHeaderFooter } from '../../layouts/FixedBackgroundHeaderFooter'
 import { imageURL } from '../../helpers/Cloudinary'
 import { postReq } from '../../helpers/HTTPRequest'
+import Avatar from '@mui/material/Avatar'
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto'
+import CloudUploadIcon from '@mui/icons-material/CloudUpload'
+import Backdrop from '@mui/material/Backdrop'
+import CircularProgress from '@mui/material/CircularProgress'
 
 const validationSchema = yup.object({
   name: yup.string().required("The artist's name is required"),
@@ -65,16 +69,23 @@ const genres = [
   'Open to ideas',
 ]
 
-export const CreateArtistProfile = ({ token }) => {
+const budgetRanges = ['$100 - $199', '$200 - $299', '$300 - $499', '$500 - $999', '$1,000+']
+
+export const CreateArtistProfile = () => {
   const [errorAlert, setErrorAlert] = useState({ show: false, message: '' })
+  const { currentUser, token } = useContext(Context)
+  const [profilePicturePreview, setProfilePicturePreview] = useState()
+  const [loading, setLoading] = useState(false)
 
   const formik = useFormik({
     initialValues: {
       name: '',
+      phone: '',
+      profile_picture: '',
+      minimum_budget: '',
       location: '',
       genre: [],
       unique_statement: '',
-      minimum_budget: '',
       type: [],
       other_venue_plays: '',
       website: '',
@@ -108,166 +119,209 @@ export const CreateArtistProfile = ({ token }) => {
 
   return (
     <FixedBackgroundHeaderFooter bgImg={imageURL('v1635338046/bg/artists_profile.jpg')}>
+      <Backdrop
+        sx={{ display: 'flex', flexDirection: 'column', color: '#fff', zIndex: theme => theme.zIndex.drawer + 1 }}
+        open={loading}
+      >
+        <CircularProgress color="primary" />
+        Creating Profile ...
+      </Backdrop>
       <Box display="flex" width="100vw">
         <Box display="flex" flexDirection="column" m="58px auto auto">
           <Box display="flex">
             <Box display="flex" flexDirection="column" className={styles.formContainer}>
-              <h1>Welcome to Spiker Booking Family!</h1>
-              <span>
-                Right On! You're now a SpikerBooking Member! The painless way to book music. Take a Few Moments to fill
-                in your artist profile page. It will help the venue operator to determine if you're a good match. Don't
-                worry fill in what you can now, you can always come back later.{' '}
+              <Collapse in={errorAlert.show}>
+                <Alert severity="error" onClose={() => setErrorAlert({ show: false, message: '' })}>
+                  <AlertTitle>{errorAlert.title}</AlertTitle>
+                  {errorAlert.message}
+                </Alert>
+              </Collapse>
+              <h1>Account Profile</h1>
+              <span className={styles.headerText}>
+                Hello {currentUser.email} welcome to Spiker Booking. Please fill out your account profile details.
               </span>
               <form onSubmit={formik.handleSubmit} className={styles.form}>
-                <Collapse in={errorAlert.show}>
-                  <Alert severity="error" onClose={() => setErrorAlert({ show: false, message: '' })}>
-                    <AlertTitle>{errorAlert.title}</AlertTitle>
-                    {errorAlert.message}
-                  </Alert>
-                </Collapse>
+                <span style={{ marginTop: '20px', textAlign: 'left' }}>
+                  Note: This information is private. It will not be shared publicly
+                </span>
                 <TextField
                   fullWidth
                   name="name"
-                  label="Artist Name*:"
+                  label="Full Name*:"
                   value={formik.values.name}
                   onChange={formik.handleChange}
                   error={formik.touched.name && Boolean(formik.errors.name)}
                   helperText={formik.touched.name && formik.errors.name}
                   margin="normal"
+                  placeholder="Jane Doe"
                 />
                 <TextField
                   fullWidth
-                  name="location"
-                  label="Location*:"
-                  value={formik.values.location}
+                  name="phone"
+                  label="Phone Number*:"
+                  value={formik.values.phone}
                   onChange={formik.handleChange}
-                  error={formik.touched.location && Boolean(formik.errors.location)}
-                  helperText={formik.touched.location && formik.errors.location}
+                  error={formik.touched.phone && Boolean(formik.errors.phone)}
+                  helperText={formik.touched.phone && formik.errors.phone}
                   margin="normal"
+                  placeholder="Use format (555) 555-5555"
                 />
-                <FormControl fullWidth margin="normal">
-                  <InputLabel id="artist-create-profile-genre">Genre (up to 5)*:</InputLabel>
-                  <Select
-                    labelId="artist-create-profile-genre"
-                    id="artist-create-profile-genre"
-                    name="genre"
-                    multiple
-                    value={formik.values.genre}
-                    onChange={formik.handleChange}
-                    error={formik.touched.genre && Boolean(formik.errors.genre)}
-                    input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-                    renderValue={selected => (
-                      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                        {selected.map(value => (
-                          <Chip key={value} label={value} />
-                        ))}
-                      </Box>
-                    )}
-                    MenuProps={MenuProps}
+                <Box display="flex" textAlign="center" justifyContent="space-between" mt="16px" mb="8px">
+                  <Avatar size="md" src={profilePicturePreview} sx={{ height: '80px', width: '80px' }} />
+                  <Button
+                    fullWidth
+                    color="secondary"
+                    variant="outlined"
+                    component="label"
+                    startIcon={<CloudUploadIcon />}
+                    sx={{ m: 'auto 0 auto 20px' }}
+                    size="large"
                   >
-                    {genres.map(name => (
-                      <MenuItem key={name} value={name}>
-                        {name}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-                <TextField
-                  fullWidth
-                  name="unique_statement"
-                  label="Unique statement*:"
-                  value={formik.values.name}
-                  onChange={formik.handleChange}
-                  error={formik.touched.unique_statement && Boolean(formik.errors.unique_statement)}
-                  helperText={formik.touched.unique_statement && formik.errors.unique_statement}
-                  margin="normal"
-                />
+                    Choose profile picture
+                    <input
+                      name="profile_picture"
+                      accept="image/*"
+                      type="file"
+                      hidden
+                      onChange={e => {
+                        formik.setFieldValue('profile_picture', e.target.files[0])
+                        const fileReader = new FileReader()
+                        fileReader.onload = () => {
+                          if (fileReader.readyState === 2) {
+                            setProfilePicturePreview(fileReader.result)
+                          }
+                        }
+                        fileReader.readAsDataURL(e.target.files[0])
+                      }}
+                    />
+                  </Button>
+                </Box>
                 <TextField
                   fullWidth
                   name="minimum_budget"
                   label="Minimum budget*:"
-                  value={formik.values.name}
+                  select
+                  value={formik.values.minimum_budget}
                   onChange={formik.handleChange}
                   error={formik.touched.minimum_budget && Boolean(formik.errors.minimum_budget)}
                   helperText={formik.touched.minimum_budget && formik.errors.minimum_budget}
                   margin="normal"
-                />
-                <FormControl fullWidth margin="normal">
-                  <InputLabel id="artist-create-profile-type">Artist type:</InputLabel>
-                  <Select
-                    labelId="artist-create-profile-type"
-                    id="artist-create-profile-type"
-                    name="type"
-                    multiple
-                    value={formik.values.type}
-                    onChange={formik.handleChange}
-                    error={formik.touched.type && Boolean(formik.errors.type)}
-                    input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-                    renderValue={selected => (
-                      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                        {selected.map(value => (
-                          <Chip key={value} label={value} />
-                        ))}
-                      </Box>
-                    )}
-                    MenuProps={MenuProps}
-                  >
-                    {artistTypes.map(name => (
-                      <MenuItem key={name} value={name}>
-                        {name}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-                <TextField
-                  fullWidth
-                  name="other_venue_plays"
-                  label="Other venue plays:"
-                  value={formik.values.other_venue_plays}
-                  onChange={formik.handleChange}
-                  error={formik.touched.other_venue_plays && Boolean(formik.errors.other_venue_plays)}
-                  helperText={formik.touched.other_venue_plays && formik.errors.other_venue_plays}
-                  margin="normal"
-                />
-                <TextField
-                  fullWidth
-                  name="website"
-                  label="Website:"
-                  value={formik.values.website}
-                  onChange={formik.handleChange}
-                  error={formik.touched.website && Boolean(formik.errors.website)}
-                  helperText={formik.touched.website && formik.errors.website}
-                  margin="normal"
-                />
-                <TextField
-                  fullWidth
-                  name="biography"
-                  label="Brief description:"
-                  multiline
-                  rows={3}
-                  value={formik.values.description}
-                  onChange={formik.handleChange}
-                  error={formik.touched.biography && Boolean(formik.errors.biography)}
-                  helperText={formik.touched.biography && formik.errors.biography}
-                  margin="normal"
-                />
-                {/*<OutlinedInput*/}
+                >
+                  {budgetRanges.map(option => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </TextField>
+                {/*<TextField*/}
                 {/*  fullWidth*/}
-                {/*  name="image"*/}
-                {/*  accept="image/*"*/}
-                {/*  type="file"*/}
-                {/*  value={formik.values.image}*/}
+                {/*  name="location"*/}
+                {/*  label="Location*:"*/}
+                {/*  value={formik.values.location}*/}
                 {/*  onChange={formik.handleChange}*/}
-                {/*  error={formik.touched.image && Boolean(formik.errors.image)}*/}
-                {/*  endAdornment={*/}
-                {/*    <InputAdornment position="end">*/}
-                {/*      <PhotoCamera />*/}
-                {/*    </InputAdornment>*/}
-                {/*  }*/}
+                {/*  error={formik.touched.location && Boolean(formik.errors.location)}*/}
+                {/*  helperText={formik.touched.location && formik.errors.location}*/}
+                {/*  margin="normal"*/}
+                {/*/>*/}
+                {/*<FormControl fullWidth margin="normal">*/}
+                {/*  <InputLabel id="artist-create-profile-genre">Genre (up to 5)*:</InputLabel>*/}
+                {/*  <Select*/}
+                {/*    labelId="artist-create-profile-genre"*/}
+                {/*    id="artist-create-profile-genre"*/}
+                {/*    name="genre"*/}
+                {/*    multiple*/}
+                {/*    value={formik.values.genre}*/}
+                {/*    onChange={formik.handleChange}*/}
+                {/*    error={formik.touched.genre && Boolean(formik.errors.genre)}*/}
+                {/*    input={<OutlinedInput id="select-multiple-chip" label="Chip" />}*/}
+                {/*    renderValue={selected => (*/}
+                {/*      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>*/}
+                {/*        {selected.map(value => (*/}
+                {/*          <Chip key={value} label={value} />*/}
+                {/*        ))}*/}
+                {/*      </Box>*/}
+                {/*    )}*/}
+                {/*    MenuProps={MenuProps}*/}
+                {/*  >*/}
+                {/*    {genres.map(name => (*/}
+                {/*      <MenuItem key={name} value={name}>*/}
+                {/*        {name}*/}
+                {/*      </MenuItem>*/}
+                {/*    ))}*/}
+                {/*  </Select>*/}
+                {/*</FormControl>*/}
+                {/*<TextField*/}
+                {/*  fullWidth*/}
+                {/*  name="unique_statement"*/}
+                {/*  label="Unique statement*:"*/}
+                {/*  value={formik.values.name}*/}
+                {/*  onChange={formik.handleChange}*/}
+                {/*  error={formik.touched.unique_statement && Boolean(formik.errors.unique_statement)}*/}
+                {/*  helperText={formik.touched.unique_statement && formik.errors.unique_statement}*/}
+                {/*  margin="normal"*/}
+                {/*/>*/}
+                {/*<FormControl fullWidth margin="normal">*/}
+                {/*  <InputLabel id="artist-create-profile-type">Artist type:</InputLabel>*/}
+                {/*  <Select*/}
+                {/*    labelId="artist-create-profile-type"*/}
+                {/*    id="artist-create-profile-type"*/}
+                {/*    name="type"*/}
+                {/*    multiple*/}
+                {/*    value={formik.values.type}*/}
+                {/*    onChange={formik.handleChange}*/}
+                {/*    error={formik.touched.type && Boolean(formik.errors.type)}*/}
+                {/*    input={<OutlinedInput id="select-multiple-chip" label="Chip" />}*/}
+                {/*    renderValue={selected => (*/}
+                {/*      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>*/}
+                {/*        {selected.map(value => (*/}
+                {/*          <Chip key={value} label={value} />*/}
+                {/*        ))}*/}
+                {/*      </Box>*/}
+                {/*    )}*/}
+                {/*    MenuProps={MenuProps}*/}
+                {/*  >*/}
+                {/*    {artistTypes.map(name => (*/}
+                {/*      <MenuItem key={name} value={name}>*/}
+                {/*        {name}*/}
+                {/*      </MenuItem>*/}
+                {/*    ))}*/}
+                {/*  </Select>*/}
+                {/*</FormControl>*/}
+                {/*<TextField*/}
+                {/*  fullWidth*/}
+                {/*  name="other_venue_plays"*/}
+                {/*  label="Other venue plays:"*/}
+                {/*  value={formik.values.other_venue_plays}*/}
+                {/*  onChange={formik.handleChange}*/}
+                {/*  error={formik.touched.other_venue_plays && Boolean(formik.errors.other_venue_plays)}*/}
+                {/*  helperText={formik.touched.other_venue_plays && formik.errors.other_venue_plays}*/}
+                {/*  margin="normal"*/}
+                {/*/>*/}
+                {/*<TextField*/}
+                {/*  fullWidth*/}
+                {/*  name="website"*/}
+                {/*  label="Website:"*/}
+                {/*  value={formik.values.website}*/}
+                {/*  onChange={formik.handleChange}*/}
+                {/*  error={formik.touched.website && Boolean(formik.errors.website)}*/}
+                {/*  helperText={formik.touched.website && formik.errors.website}*/}
+                {/*  margin="normal"*/}
+                {/*/>*/}
+                {/*<TextField*/}
+                {/*  fullWidth*/}
+                {/*  name="biography"*/}
+                {/*  label="Brief description:"*/}
+                {/*  multiline*/}
+                {/*  rows={3}*/}
+                {/*  value={formik.values.description}*/}
+                {/*  onChange={formik.handleChange}*/}
+                {/*  error={formik.touched.biography && Boolean(formik.errors.biography)}*/}
+                {/*  helperText={formik.touched.biography && formik.errors.biography}*/}
+                {/*  margin="normal"*/}
                 {/*/>*/}
                 <div style={{ margin: '32px 0 0 auto' }}>
                   <Button color="primary" variant="contained" size="large" type="submit" disabled>
-                    Finish (WIP)
+                    Next (WIP)
                   </Button>
                 </div>
               </form>
