f6f5e389174a36af7e0f0e07c0b9412791daa3e7
Mina Nabil <minanabilsalib@gmail.com>
2022-01-26 20:42:04 +0200

    formatting code with prettier

--- app/javascript/pages/ArtistDetails/index.jsx
+++ app/javascript/pages/ArtistDetails/index.jsx
@@ -51,7 +51,10 @@ export const ArtistDetails = () => {
                 })}
               />
             </div>
-            <Button fullWidth variant="contained" style={{ marginTop: '16px' }}
+            <Button
+              fullWidth
+              variant="contained"
+              style={{ marginTop: '16px' }}
               onClick={() => (window.location = 'https://www.spikerbooking.com/book-now')}
             >
               Book Now
